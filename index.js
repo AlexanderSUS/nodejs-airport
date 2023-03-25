c71d6b1e84867af21320c19884c0c71f03c83209
Alexander Suslov <sasha_suslov07@mail.ru>
2023-03-25 12:11:07 +0300

    feat: add prefix and version

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ dotenv.config();
 const PORT = process.env.APP_PORT || 4000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello world!');
+  if (req.url === '/api/v1') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello world!');
+  } else {
+    res.writeHead(404,{ 'Content-Type': 'application/json'} )
+    res.write(JSON.stringify({ message: `Could not ${req.method} ${req.url}`}));
+    res.end();
+  }
 });
 
 server.on('listening', () => {
