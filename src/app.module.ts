0473b8e0ebafb9b1d338dfa78a62d48033ad7037
Alexander Suslov <sasha_suslov07@mail.ru>
2023-03-25 15:39:01 +0300

    feat: create basic database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configModuleOptions } from './config/config-module-options.config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(configModuleOptions)],
+  imports: [ConfigModule.forRoot(configModuleOptions), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
