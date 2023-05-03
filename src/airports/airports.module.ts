6a4f24bffcd8b822b81c13171b184af388437317
solvd <solvd@solvds-MacBook-Pro.local>
2023-05-03 14:34:56 +0300

    fix: import

--- src/airports/airports.module.ts
+++ src/airports/airports.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AirportsService } from './airports.service';
 import { AirportsController } from './airports.controller';
+import { AirportsRepository } from './airports.repository';
 
 @Module({
   controllers: [AirportsController],
-  providers: [AirportsService],
+  providers: [AirportsService, AirportsRepository],
 })
 export class AirportsModule {}
