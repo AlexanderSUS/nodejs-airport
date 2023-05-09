98ecde4314f9ed17f27ba29417180c2fd9c9db7e
Alexander Suslov <sasha_suslov07@mail.ru>
2023-05-09 22:30:43 +0300

    fix: flights-search model

--- src/flights-search/flights-search.model.ts
+++ src/flights-search/flights-search.model.ts
@@ -3,6 +3,8 @@ import { Expose } from 'class-transformer';
 export class FlightsSearchModel {
   date: string;
 
+  cost: string;
+
   @Expose({ name: 'departure_country' })
   departureCountry: string;
 
